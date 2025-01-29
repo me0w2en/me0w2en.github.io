610fe578603a0433c140df4c92725eaad6e5180e
me0w2en <sjna@outlook.kr>
2025-01-29 11:49:53 +0900

    Design: Footer 배경 색상 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-4">
+    <footer className="bg-black text-white py-4">
       <div className="container mx-auto text-center">
         <p className="text-sm">
           &copy; {new Date().getFullYear()} Your Name. All rights reserved.
