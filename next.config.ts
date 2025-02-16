62257a752207307e90e2a083e107893a56aa2510
me0w2en <sjna@outlook.kr>
2025-02-16 19:29:30 +0900

    Chore: deploy option 수정

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,11 @@ const nextConfig: NextConfig = {
   output: 'export',
   images:{
     unoptimized:true,
-  }
+  },
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://me0w2en.github.io/"
+      : "",
 };
 
 export default nextConfig;
