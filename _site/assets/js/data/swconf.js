b6a9d9c4ede6ab34527da3fb9010740d5180d379
me0w2en <sjna@outlook.kr>
2025-06-30 11:52:38 +0900

    Fix: Modify naming and mail address

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1751251104',resources: [
+    cacheName: 'chirpy-1751251861',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
